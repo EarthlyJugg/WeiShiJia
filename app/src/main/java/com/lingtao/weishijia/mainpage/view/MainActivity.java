c56c273e1f985ad3f85743a92fbbca35abdff780
lingtaoyulian <7715482292@qq.com>
2016-11-07 16:31:35 +0800

    a

--- app/src/main/java/com/lingtao/weishijia/mainpage/view/MainActivity.java
+++ app/src/main/java/com/lingtao/weishijia/mainpage/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.lingtao.weishijia;
+package com.lingtao.weishijia.mainpage.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lingtao.weishijia.R;
 
 public class MainActivity extends AppCompatActivity {
 
